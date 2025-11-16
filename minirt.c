5a3770dd0f8faa72c0cbe1960ccc45a2134e6740
jlima-olive <namejojo0214@gmail.com>
2025-11-16 18:56:07 +0000

    update

--- minirt.c
+++ minirt.c
@@ -6,7 +6,7 @@
 /*   By: jlima-so <jlima-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 13:48:56 by namejojo          #+#    #+#             */
-/*   Updated: 2025/11/16 18:06:28 by jlima-so         ###   ########.fr       */
+/*   Updated: 2025/11/16 18:14:44 by jlima-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -690,12 +690,130 @@ int	find_ligh(t_mlximg img, t_ray ray)
 	return (0);
 }
 
-int	main(void)
+static void	init_scene(t_scene *scene)
+{
+	scene->ambient = NULL;
+	scene->camera = NULL;
+	scene->light = NULL;
+	scene->list = NULL;
+	scene->n_objects = 0;
+}
+
+static void	print_color(const t_rgb *c)
+{
+	printf("(%f, %f, %f)", c->x, c->y, c->z);
+}
+
+static void	print_vec(const t_vec *v)
+{
+	printf("(%.3f, %.3f, %.3f)", v->x, v->y, v->z);
+}
+
+void print_scene(const t_scene *scene)
+{
+    t_list *node;
+
+    printf("----- SCENE DEBUG -----\n");
+
+    if (scene->ambient)
+    {
+        printf("Ambient:\n");
+        printf("  Ratio: %.3f\n", scene->ambient->ratio);
+        printf("  Color: ");
+        print_color(&scene->ambient->color);
+        printf("\n");
+    }
+    else
+        printf("Ambient: (null)\n");
+
+    if (scene->camera)
+    {
+        printf("Camera:\n");
+        printf("  Position: ");
+        print_vec(&scene->camera->src);
+        printf("\n  Direction: ");
+        print_vec(&scene->camera->dir);
+        printf("\n  FOV: %.2f\n", scene->camera->fov);
+    }
+    else
+        printf("Camera: (null)\n");
+
+    if (scene->light)
+    {
+        printf("Light:\n");
+        printf("  Position: ");
+        print_vec(&scene->light->src);
+        printf("\n  Brightness: %.3f\n", scene->light->brightness);
+        printf("  Color: ");
+        print_color(&scene->light->color);
+        printf("\n");
+    }
+    else
+        printf("Light: (null)\n");
+
+    // Print spheres
+    node = scene->list;
+    while (node)
+    {
+        if (node->type == SPHERE)
+        {
+            t_sphere *sp = node->obj;
+            printf("Sphere:\n");
+            printf("  Center: ");
+            print_vec(&sp->center);
+            printf("\n  Radius: %.3f\n", sp->r);
+            printf("  Color: ");
+            print_color(&sp->color);
+            printf("\n  Material: %d\n", sp->material);
+        }
+		if (node->type == PLANE)
+        {
+            t_plane *pl = node->obj;
+            printf("Plane:\n");
+            printf("  Point: ");
+            print_vec(&pl->point);
+            printf("\n  Normal: ");
+        		print_vec(&pl->normal);
+            printf("\n  Color: ");
+            print_color(&pl->color);
+            printf("\n  Material: %d\n", pl->material);
+        }
+		if (node->type == CYLINDER)
+		{
+			t_cylinder *cy = node->obj;
+			printf("Cylinder:\n");
+			printf("  Center: ");
+			print_vec(&cy->center);
+			printf("\n  Axis: ");
+			print_vec(&cy->axis);
+			printf("\n  Radius: %.3f\n", cy->r);
+			printf("  Height: %.3f\n", cy->height);
+			printf("  Color: ");
+			print_color(&cy->color);
+			printf("\n  Material: %d\n", cy->material);
+		}
+        node = node->next;
+    }
+	
+    printf("-----------------------\n");
+}
+
+int	main(int argc, char **argv)
 {
 	t_mlx	mlx;
 	t_point	camera_center;
 	t_point	pixel;
+/********************************/
+	t_scene	scene;
 
+	if (!precheck(argc, argv[1]))
+		return (1);
+	init_scene(&scene);
+	parse(argv[1], &scene);
+	print_scene(&scene);
+	clean_scene(&scene);
+	return (0);
+/*************************************************/
 	init_var(&mlx);
 	if (init_mlx(&mlx))
 		return (1);
